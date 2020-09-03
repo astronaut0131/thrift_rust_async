5f855df825d55c340d98fad2ca4969d8a71dfb73
guajialin <1406147701@qq.com>
2020-09-04 01:04:57 +0800

    fix bug: server not implement method echo

--- benchmark/src/async_thrift_test/server.rs
+++ benchmark/src/async_thrift_test/server.rs
@@ -1,9 +1,4 @@
-use std::io::Error;
-
-use async_std::{
-    net::ToSocketAddrs,
-    task,
-};
+use async_std::{net::ToSocketAddrs, task, io};
 use async_trait::async_trait;
 
 use async_thrift::protocol::async_binary::{TAsyncBinaryInputProtocolFactory, TAsyncBinaryOutputProtocolFactory};
@@ -12,9 +7,10 @@ use async_thrift::transport::async_buffered::{TAsyncBufferedReadTransportFactory
 use async_thrift::transport::async_framed::{TAsyncFramedReadTransportFactory, TAsyncFramedWriteTransportFactory};
 
 use crate::async_thrift_test::tutorial::{CalculatorSyncHandler, CalculatorSyncProcessor};
+use crate::async_thrift_test::echo::{LongMessageTestSyncProcessor, LongMessageTestSyncHandler};
 
 pub async fn run_server(addr: String) {
-    let processor = CalculatorSyncProcessor::new(PartHandler {});
+    let processor = LongMessageTestSyncProcessor::new(PartHandler {});
     let r_trans_factory = TAsyncBufferedReadTransportFactory::new();
     let w_trans_factory = TAsyncBufferedWriteTransportFactory::new();
     let i_proto_factory = TAsyncBinaryInputProtocolFactory::new();
@@ -27,8 +23,17 @@ pub async fn run_server(addr: String) {
 struct PartHandler {}
 
 #[async_trait]
-impl CalculatorSyncHandler for PartHandler {
-    async fn handle_ping(&self) -> async_thrift::Result<()> {
-        Ok(())
+// impl CalculatorSyncHandler for PartHandler {
+//     async fn handle_ping(&self) -> async_thrift::Result<()> {
+//         Ok(())
+//     }
+// }
+impl LongMessageTestSyncHandler for PartHandler {
+    // async fn handle_ping(&self) -> async_thrift::Result<()> {
+    //     Ok(())
+    // }
+
+    async fn handle_echo(&self, input: Vec<i8>) -> async_thrift::Result<Vec<i8>> {
+        Ok(input)
     }
 }
