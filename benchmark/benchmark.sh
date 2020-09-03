11a76dc8e8a15fbd03a41a87c6f2fa40814cd4af
root <root@debug010000002015.local.alipay.net>
2020-09-03 18:56:51 +0800

    change environment ASYNC_STD_THREAD_COUNT to  * 2

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,13 +1,18 @@
-export ASYNC_STD_THREAD_COUNT=128
+#!/bin/sh
+
+# set environment $ASYNC_STD_THREAD_COUNT t0 $CPU_NUM
+CPU_NUM=$(($(cat /proc/cpuinfo |grep "processor"|wc -l) * 2));
+export ASYNC_STD_THREAD_COUNT=$CPU_NUM;
+echo "SET ASYNC_STD_THREAD_COUNT = $ASYNC_STD_THREAD_COUNT";
 
 ## config parameter
-RUN_CLIENT=true
+RUN_CLIENT=true;
 RUN_SERVER=true;
 RUN_SYNC=true;
 RUN_ASYNC=true;
 RUN_ASYNC_TOKIO=false;
-THREAD_NUM=200
-LOOP_NUM=1000
+THREAD_NUM=200;
+LOOP_NUM=1000;
 ADDR=127.0.0.1:9090;
 ##
 
